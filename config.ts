6434bfda3ab7e7f40addb940dc6ef829a46e55f7
iberaza1 <iberaza1@gmail.com>
2026-01-07 20:52:16 -0600

    Updated Maps API key

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 // config.ts
 // Updated to match the Firebase project key for velocity-tracker-483022-t5
-export const GOOGLE_MAPS_API_KEY = "AIzaSyBd_3dUxlmARJ9lLsxrda475AZV7vwqJ3w";
+export const GOOGLE_MAPS_API_KEY = "AIzaSyABpdVJFvudPJTf2UV7sYkbmlxPirebJ-A";
