c20cecb8a148f802742074a1071380a504e97e7a
iberaza1 <iberaza1@gmail.com>
2026-01-07 20:50:53 -0600

    Updated API Key

--- services/firebase.ts
+++ services/firebase.ts
@@ -6,7 +6,7 @@ import { getFirestore } from 'firebase/firestore';
  * Successfully integrated with velocity-tracker-483022-t5
  */
 const firebaseConfig = {
-  apiKey: "AIzaSyBd_3dUxlmARJ9lLsxrda475AZV7vwqJ3w",
+  apiKey: "AIzaSyABpdVJFvudPJTf2UV7sYkbmlxPirebJ-A",
   authDomain: "velocity-tracker-483022-t5.firebaseapp.com",
   projectId: "velocity-tracker-483022-t5",
   storageBucket: "velocity-tracker-483022-t5.firebasestorage.app",
